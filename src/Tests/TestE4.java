3f4391d688a0366c4beb69e564efaa6346451f8d
Yuuta094 <YvanSan094@gmail.com>
2022-10-09 18:14:46 +0200

    Ejercicio_4: Terminado

--- src/Tests/TestE4.java
+++ src/Tests/TestE4.java
@@ -1,9 +1,49 @@
 package Tests;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import us.lsi.common.Files2;
+
 public class TestE4 {
 	
 	public static void main(String[] args) {
-
+		
+		String file= "datos/PI1Ej4DatosEntrada.txt";
+		
+		List<List<Integer>> datos = lee_fichero(file); 
+		
+		for (int i= 0; i<datos.size()-1; i++) { 
+			
+			Integer a= datos.get(i).get(0);  
+			Integer b= datos.get(i).get(1);
+			Integer c= datos.get(i).get(2);
+			
+//Me da error de "Java heap space en la última entrada (60, 50, 40)
+			
+		String RecSinMemoria = ejercicio4.RecursivaSinMemoriaE4.ejercicio4_RecSinMemoria(a, b, c);
+		String RecConMemoria = ejercicio4.RecursivaConMemoriaE4.ejercicio4_RecConMemoria(a, b, c);
+		String Iterativa = ejercicio4.IterarivaE4.ejercicio4Iterativo(a, b, c);
+		
+		System.out.println("Entrada: " + a + ", " + b + ", " + c + "\n" +
+						   "ConM: " + RecSinMemoria + "\n" +
+						   "SinM: "+ RecConMemoria + "\n" +
+						   "Iter: "+ Iterativa + "\n");
+		}
+	}
+	
+	public static List<List<Integer>> lee_fichero(String ruta) {
+		List<String> lineas = Files2.linesFromFile(ruta);
+		List<List<Integer>> res = new ArrayList<List<Integer>>();
+		for (String l: lineas) {
+			List<String> num = Arrays.asList(l.split(","));
+			List<Integer> ListaNum = List.of(((Integer.parseInt(num.get(0)))),
+											  Integer.parseInt(num.get(1)),
+											  Integer.parseInt(num.get(2)));
+			res.add(ListaNum);
+		}
+		return res;
 	}
 
 }
--- src/ejercicio4/IterarivaE4.java
+++ src/ejercicio4/IterarivaE4.java
@@ -1,5 +1,42 @@
 package ejercicio4;
 
-public class IterarivaE4 {
+import java.util.Map;
 
-}
+import ejercicio4.RecursivaConMemoriaE4.Trio;
+import us.lsi.common.Map2;
+
+public class IterarivaE4 {
+	
+	public static String ejercicio4Iterativo (Integer a, Integer b, Integer c) {
+		
+		//Me da pero falla algo
+		
+		Map<Trio, String> map = Map2.empty();
+		String s = map.get(Trio.of(a,b,c));
+		
+		for (int i=0; i<a+1; i++) {
+			for (int j=0; j<b+1;j++) {
+				for (int k=0;k<c+1;k++) {
+			
+			if (i < 2 && j<=2 || k < 2) {
+				s= "(" + i + "+" + j + "+" + k +")";	
+			}
+			else if (i < 3 || j < 3 && k <= 3 ) {
+				s= "(" + i + "-" + j + "-" + k +")";
+			}
+			else if (j%i==0 && (i%2 ==0 || j%3 == 0)) {
+				s= "(" + map.get(Trio.of(i-1, j/i, k-1)) + "*" + map.get(Trio.of(i-2, j/2, k-2)) + ")";
+			}
+			else {
+				s= "(" + map.get(Trio.of(i/2, j-2, k/2)) + "/" + map.get(Trio.of(i/3, j-1, k/3)) + ")";
+				
+						}
+				map.put(Trio.of(i, j, k), s);
+					}
+				}
+			}
+		
+		return s;
+	}
+
+ }
